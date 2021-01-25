e0d4ceb57db7b3315531e01f03754eae43282e23
Dominic Pajak <8bitkick@users.noreply.github.com>
2021-01-24 21:59:07 -0800

    Update makeUEF.js

--- makeUEF.js
+++ makeUEF.js
@@ -1,47 +1,147 @@
+const fs = require('fs');
 
-  // Acorn Cassette tape format
-  // See http://bbc.nvg.org/doc/BBCUserGuide-1.00.pdf Chapter 35
+var crcTable = [0x0000, 0x1021, 0x2042, 0x3063, 0x4084, 0x50a5,
+  0x60c6, 0x70e7, 0x8108, 0x9129, 0xa14a, 0xb16b,
+  0xc18c, 0xd1ad, 0xe1ce, 0xf1ef, 0x1231, 0x0210,
+  0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6,
+  0x9339, 0x8318, 0xb37b, 0xa35a, 0xd3bd, 0xc39c,
+  0xf3ff, 0xe3de, 0x2462, 0x3443, 0x0420, 0x1401,
+  0x64e6, 0x74c7, 0x44a4, 0x5485, 0xa56a, 0xb54b,
+  0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d,
+  0x3653, 0x2672, 0x1611, 0x0630, 0x76d7, 0x66f6,
+  0x5695, 0x46b4, 0xb75b, 0xa77a, 0x9719, 0x8738,
+  0xf7df, 0xe7fe, 0xd79d, 0xc7bc, 0x48c4, 0x58e5,
+  0x6886, 0x78a7, 0x0840, 0x1861, 0x2802, 0x3823,
+  0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969,
+  0xa90a, 0xb92b, 0x5af5, 0x4ad4, 0x7ab7, 0x6a96,
+  0x1a71, 0x0a50, 0x3a33, 0x2a12, 0xdbfd, 0xcbdc,
+  0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a,
+  0x6ca6, 0x7c87, 0x4ce4, 0x5cc5, 0x2c22, 0x3c03,
+  0x0c60, 0x1c41, 0xedae, 0xfd8f, 0xcdec, 0xddcd,
+  0xad2a, 0xbd0b, 0x8d68, 0x9d49, 0x7e97, 0x6eb6,
+  0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0x0e70,
+  0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a,
+  0x9f59, 0x8f78, 0x9188, 0x81a9, 0xb1ca, 0xa1eb,
+  0xd10c, 0xc12d, 0xf14e, 0xe16f, 0x1080, 0x00a1,
+  0x30c2, 0x20e3, 0x5004, 0x4025, 0x7046, 0x6067,
+  0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c,
+  0xe37f, 0xf35e, 0x02b1, 0x1290, 0x22f3, 0x32d2,
+  0x4235, 0x5214, 0x6277, 0x7256, 0xb5ea, 0xa5cb,
+  0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
+  0x34e2, 0x24c3, 0x14a0, 0x0481, 0x7466, 0x6447,
+  0x5424, 0x4405, 0xa7db, 0xb7fa, 0x8799, 0x97b8,
+  0xe75f, 0xf77e, 0xc71d, 0xd73c, 0x26d3, 0x36f2,
+  0x0691, 0x16b0, 0x6657, 0x7676, 0x4615, 0x5634,
+  0xd94c, 0xc96d, 0xf90e, 0xe92f, 0x99c8, 0x89e9,
+  0xb98a, 0xa9ab, 0x5844, 0x4865, 0x7806, 0x6827,
+  0x18c0, 0x08e1, 0x3882, 0x28a3, 0xcb7d, 0xdb5c,
+  0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a,
+  0x4a75, 0x5a54, 0x6a37, 0x7a16, 0x0af1, 0x1ad0,
+  0x2ab3, 0x3a92, 0xfd2e, 0xed0f, 0xdd6c, 0xcd4d,
+  0xbdaa, 0xad8b, 0x9de8, 0x8dc9, 0x7c26, 0x6c07,
+  0x5c64, 0x4c45, 0x3ca2, 0x2c83, 0x1ce0, 0x0cc1,
+  0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba,
+  0x8fd9, 0x9ff8, 0x6e17, 0x7e36, 0x4e55, 0x5e74,
+  0x2e93, 0x3eb2, 0x0ed1, 0x1ef0];
 
-  function blockData(filename, loadAddress, execAddress, payload, block, eof){
-    let data = [];
+  // https://gist.github.com/chitchcock/5112270
+  function crc16(s) {
+    var crc = 0xFFFF;
+    var j, i;
+    for (i = 0; i < s.length; i++) {
+      let c = (typeof s === "string") ? s.charCodeAt(i) & 0xff: s[i] & 0xff;
+      if (c > 255) {
+        throw new RangeError();
+      }
+      j = (c ^ (crc >> 8)) & 0xFF;
+      crc = crcTable[j] ^ (crc << 8);
+    }
+    return ((crc ^ 0) & 0xFFFF);
+  }
 
-    function addByte(b){data.push(b)};
-    function addDouble(a){for (let b=0; b<4; b++){data.push(a & 0xff);a>>8}};
-    function addWord(a){for (let b=0; b<2; b++){data.push(a & 0xff);a>>8}};
-    function addString(s){for (let a=0; a<s.length; a++){data.push(s.charCodeAt(a));}};
-    function addData(d){data.concat(d);};
-    function addCRC(d){
-      let h = 0;
-      let l = 0;
-      for (let a=0; a<d.length; a++){
-        let c = (typeof d === "String") ? d.charCodeAt(a) : d[a];
-        h = c ^ h;
-        for (let x=1; x==8; x++){
-          let t = 0
-          if ((h & 1<<7) == 1<<7) {
-            h=h^0x08;
-            l=l^0x10;
-            t=1;
-          }
-          let j = (((h<<8 | l)*2+t) & 0xffff);
-          h = j >> 8; l = j & 0xff;
+  Array.prototype.add = function(d, l){
+    if (l) {
+      for (let b=0; b<l; b++){
+        this.push(d & 0xff);
+        d>>=8}
+      } else {
+        for (let a=0; a<d.length; a++){
+          let c = (typeof d === "string") ? d.charCodeAt(a) & 0xff: d[a] & 0xff;
+          this.push(c);
         }
       }
-      data.push(h);
-      data.push(l);
     }
 
-    addByte(0x2A);          // One synchronisation byte (&2A).
-    addString(filename);    // File name (one to ten characters).
-    addByte(0x00);          // One end of file name marker byte (&00).
-    addDouble(loadAddress); // Load address of file, four bytes, low byte first.
-    addDouble(execAddress); // Execution address of file, four bytes, low byte first.
-    addWord(block);         // Block number, two bytes, low byte first.
-    addWord(payload.length);// Data block length, two bytes, low byte first.
-    addByte(eof ? 1<<7 : 0);// Block flag (Bit 7), one byte.
-    addWord(0);             // Spare, four bytes, currently &00.
-    addCRC(data.slice(1,data.length)); // CRC on header, two bytes.
-    addString(payload);       // Data, 0 to 256 bytes.
-    addCRC(payload);        // CRC on data, two bytes.
-    return data;
-  }
+    function makeAcornBlock(filename, loadAddress, execAddress, payload, blockNumber, eof){
+      let block = [];
+
+      // header
+      block.add(0x2A,1);                            // One synchronisation byte (&2A).
+      block.add(filename);                          // File name (one to ten characters).
+      block.add(0x00,1);                            // One end of file name marker byte (&00).
+      block.add(loadAddress,4);                     // Load address of file, four bytes, low byte first.
+      block.add(execAddress,4);                     // Execution address of file, four bytes, low byte first.
+      block.add(blockNumber,2);                     // Block number, two bytes, low byte first.
+      block.add(payload.length,2);                  // Data block length, two bytes, low byte first.
+      block.add(eof ? 1<<7 : 0);                    // Block flag (Bit 7), one byte.
+      block.add(0,4);                               // Spare, four bytes, currently &00.
+      let headerCRC = crc16(block.slice(1,block.length));
+      block.add(headerCRC,2);                       // CRC on header, two bytes.
+
+      // data
+      block.add(payload);
+      let dataCRC = crc16(payload);
+      block.add(dataCRC,2);                         // CRC on data, two bytes.
+      return block;
+    }
+
+    function makeUEF(filename, loadAddress, execAddress, payload){
+      let UEF = [];
+      let blockNumber = 0;
+      let blockStart = 0;
+      let blockEnd = 0;
+
+      UEF.add("UEF File!");
+      UEF.add([0,0,0]);
+
+      UEF.add(0x0110        ,2); // carrier tone
+      UEF.add(2             ,4); // chunk length
+      UEF.add(1500          ,2); // chunk length
+
+      while(blockStart < payload.length-1){
+        let EOF = false
+        let blockEnd = blockStart+255;
+
+        if (blockEnd>=payload.length-1) {
+          blockEnd = payload.length-1;
+          EOF=true;
+        }
+
+        let blockData = payload.slice(blockStart, blockEnd+1);
+        console.log(blockData+"\n\n")
+
+        let block = makeAcornBlock(filename, loadAddress, execAddress, blockData, blockNumber, EOF);
+
+        UEF.add(0x0110        ,2); // carrier tone
+        UEF.add(2             ,4); // chunk length
+        UEF.add(600           ,2); // chunk length
+
+        UEF.add(0x0100        ,2); // data block
+        UEF.add(block.length  ,4); // chunk length
+        UEF.add(block);            // block data
+
+        blockStart=blockEnd+1;
+        blockNumber++;
+      }
+
+      UEF.add(0x0110        ,2); // carrier tone
+      UEF.add(2             ,4); // chunk length
+      UEF.add(1500          ,2); // chunk length
+
+      return UEF;
+    }
+
+    let basic = fs.readFileSync('./tweet.bas','utf8');
+    let uef = makeUEF("TWEET", 0x1900, 0x1900, basic);
+
+    fs.writeFileSync('./tweet.uef',new Uint8Array(uef));
