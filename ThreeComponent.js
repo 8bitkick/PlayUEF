efc8a1bbb5999b798b7a7a904bd8f9de45a3d97f
8bitkick <8bitkick@gmail.com>
2023-07-04 22:58:23 +0200

    Player

--- ThreeComponent.js
+++ ThreeComponent.js
@@ -9,14 +9,20 @@ const Model = ({ url }) => {
 
     // Set rotation values for x, y, and z axis.
     const rotation = [ Math.PI / 2,  -Math.PI / 2,0]; // Rotate 90 degrees around Y axis
-    const position = [ -2.25,0,-8];
+    const position = [ -2,0,0];
     return <primitive object={gltf.scene} rotation={rotation} position={position} />;
 };
 
 const ThreeComponent = () => (
   <div style={{ width: '100%', height: '640px' }}> {/* Set the size of the div */}
-      <Canvas camera={{ fov: 35 }} style={{ width: '100%', height: '100%' }}>
-          <ambientLight />
+      <Canvas
+      style={{ background: 'blue' }}
+       gl={{ alpha: true, antialias: true, logarithmicDepthBuffer: true, bgColor: 'blue' }}
+      camera={{ fov: 35, position: [-2,0,12]  }} style={{ width: '100%', height: '100%' }}>
+
+          <ambientLight intensity={0.5} /> {/* Base light */}
+  <directionalLight position={[0, 10, 0]} intensity={1}/> {/* Simulate sun light */}
+  <spotLight position={[10, 15, 10]} angle={0.3} intensity={3}/> {/* Highlight specific areas */}
           <Suspense fallback={<Html><Loader /></Html>}>
               <Model url={cassetteGLB} />
           </Suspense>
--- index.js
+++ index.js
@@ -5,10 +5,12 @@ import { useLocation } from 'react-router-dom';
 import uef2wave from './uef2wave';
 import { handleZip } from './utils.js';
 import ThreeComponent from './ThreeComponent';
+import Player from './Player';
 
 function PlayUEFwrap() {
 
   const [wavFile, setWavFile] = useState('');
+  const [UEF, setUEF] = useState('');
   const location = useLocation();
   const urlParams = new URLSearchParams(location.search);
 
@@ -17,43 +19,46 @@ function PlayUEFwrap() {
     if (FILE) {
       downloadUEF(FILE);
     }
-    }, []);
+  }, []);
 
 
-    const downloadUEF = async (FILE) => {
-      try {
-        const SAMPLE_RATE = urlParams.get("SAMPLE_RATE") || 48000
-        const LOW       = urlParams.get("LOW") || 1200;
-        const TURBO     = urlParams.get("TURBO") || 0;
-        const PHASE     = (urlParams.get("PHASE") || 180)*(Math.PI/180);
-        const LOCAL     = urlParams.get("LOCAL") || false;
-        const CARRIER   = (urlParams.get("CARRIER") || 2)/2;
-        const STOPBIT   = urlParams.get("STOPBIT") || 4;
-        const HIGH      = urlParams.get("HIGH") || LOW * 2;
-        const DATA      = urlParams.get("DATA") || false;
-        const BAUD      = urlParams.get("BAUD") || Math.floor((parseInt(LOW) + parseInt(HIGH)) / 2 / 2);
+  const downloadUEF = async (FILE) => {
+    try {
+      const SAMPLE_RATE = urlParams.get("SAMPLE_RATE") || 48000
+      const LOW       = urlParams.get("LOW") || 1200;
+      const TURBO     = urlParams.get("TURBO") || 0;
+      const PHASE     = (urlParams.get("PHASE") || 180)*(Math.PI/180);
+      const LOCAL     = urlParams.get("LOCAL") || false;
+      const CARRIER   = (urlParams.get("CARRIER") || 2)/2;
+      const STOPBIT   = urlParams.get("STOPBIT") || 4;
+      const HIGH      = urlParams.get("HIGH") || LOW * 2;
+      const DATA      = urlParams.get("DATA") || false;
+      const BAUD      = urlParams.get("BAUD") || Math.floor((parseInt(LOW) + parseInt(HIGH)) / 2 / 2);
 
-        if (TURBO==1) {STOPBIT=1; CARRIER=0; LOW = 1280}
-        // Use appropriate method (e.g., fetch) to download the UEF file
-        const fileData = await fetch(FILE).then((response) => response.arrayBuffer());
+      if (TURBO==1) {STOPBIT=1; CARRIER=0; LOW = 1280}
+      // Use appropriate method (e.g., fetch) to download the UEF file
+      const fileData = await fetch(FILE).then((response) => response.arrayBuffer());
 
-        let uef = await handleZip(fileData, FILE);
+      let uef = await handleZip(fileData, FILE);
 
-        // Call the `uef2wave` function to convert UEF to WAV
-        const output = await uef2wave(Array.from(uef.data), LOW, SAMPLE_RATE, STOPBIT, PHASE, CARRIER, HIGH);
-        const blob = new Blob([output.wav], { type: 'audio/wav' });
-        const url = window.URL.createObjectURL(blob);
-setWavFile(url);
+      // Call the `uef2wave` function to convert UEF to WAV
+      const output = await uef2wave(Array.from(uef.data), LOW, SAMPLE_RATE, STOPBIT, PHASE, CARRIER, HIGH);
+      const blob = new Blob([output.wav], { type: 'audio/wav' });
+      const url = window.URL.createObjectURL(blob);
+      setWavFile(url);
+      setUEF(output.uef);
 
-      } catch (error) {
-        console.error('Error converting UEF to WAV:', error);
-      }
-    };
+    } catch (error) {
+      console.error('Error converting UEF to WAV:', error);
+    }
+  };
 
   return (
     <div>
     {wavFile && <ThreeComponent />}
-      {wavFile && <audio src={wavFile} controls />}
+    
+          {wavFile && <Player src={wavFile} uef={UEF} />}
+
     </div>
   );
 }
