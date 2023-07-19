b8dfe769c61ecdde302c3f53c137ff3bfaa5aac0
8bitkick <8bitkick@gmail.com>
2023-07-19 23:55:25 +0200

    Hex dump

--- player.js
+++ player.js
@@ -6,7 +6,7 @@ const Player = ({ src, uef, baud, sampleRate }) => {
   const audioRef = useRef();
   const [currentTime, setCurrentTime] = useState(0);
   const [currentText, setCurrentText] = useState("");
-  const [currentHeader, setCurrentHeader] = useState("");
+  const [currentHeader, setCurrentHeader] = useState(".");
   const [totalLength, setTotalLength] = useState(0);
   const [playerState, setPlayerState] = useState("");
 
@@ -23,13 +23,41 @@ const Player = ({ src, uef, baud, sampleRate }) => {
   useEffect(() => {
     const audio = audioRef.current;
 
+    const stringToHexDump = (str) => {
+        let hexDump = "";
+        let lineLength = 0;
+        let charSnippet = "";
+        let hexLine = "";
+
+        for (let i = 0; i < str.length; i++) {
+            hexLine += (0xff & str.charCodeAt(i)).toString(16).padStart(2, '0').toUpperCase() + " ";
+            charSnippet += str[i];
+
+            lineLength++;
+            if (lineLength === 16 || i === str.length - 1) {
+                // Pad the hex line if it's less than 32 characters
+                while (lineLength < 16) {
+                    hexLine += "   "; // 2 spaces for the hex and 1 space as separator
+                    charSnippet += " ";
+                    lineLength++;
+                }
+                hexDump += hexLine + "  " + charSnippet + '\n';
+                lineLength = 0;
+                charSnippet = "";
+                hexLine = "";
+            }
+        }
+        return hexDump;
+    };
+
+
     const updateTime = () => {
       setCurrentTime(audio.currentTime);
       checkPlayerState();
-      const relevantText = chunkAtTime(audio.currentTime, uef, baud, sampleRate);
+     let relevantText = chunkAtTime(audio.currentTime, uef, baud, sampleRate);
     console.log(relevantText)
       if(relevantText){
-        setCurrentText(relevantText.str);
+        setCurrentText(stringToHexDump(relevantText.str));
         setCurrentHeader(relevantText.header);
       }
     };
